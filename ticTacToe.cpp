38bdd55da9d080f61baba46c1cccd5afa85c932d
Michael H <148577314+mhargreaCS@users.noreply.github.com>
2024-01-12 14:59:48 -0500

    Update ticTacToe.cpp
    All basic functions completed except a threeInARow winning condition checker

--- ticTacToe.cpp
+++ ticTacToe.cpp
@@ -26,14 +26,13 @@ int main() {
 	const int cols = 5;
 
 	char board[rows][cols] = { { ' ','|', ' ', '|', ' ' },
-									          {'-', '-', '-',  '-', '-'},
-									          { ' ','|', ' ', '|', ' ' },
-									          {'-', '-', '-',  '-', '-'},
-									          { ' ','|', ' ', '|', ' ' } };
-
-	functions::print(board, rows, cols);
+							 {'-', '-', '-',  '-', '-'},
+							 { ' ','|', ' ', '|', ' ' },
+							 {'-', '-', '-',  '-', '-'},
+							 { ' ','|', ' ', '|', ' ' } };
 
 	int counter = 0;
+	int gameLimit = 0;
 	do {
 		functions::print(board, rows, cols);
 
@@ -47,22 +46,120 @@ int main() {
 			counter++;
 		}
 
+		char& current = currentPlayer;
+
 		std::cout << "It is " << currentPlayer << "'s turn. Where would you like to play?: " << std::endl;
 		int input;
 		std::cin >> input;
 
-		switch (input) {
+		bool validInput = true; int counter = 0;
+		do {
+			if (counter == 1) {
+				std::cout << "Invalid location, please enter another location:" << std::endl;
+				std::cin >> input;
+				counter--;
+			}
+			switch (input) {
 			case 1:
-				board[0][0] = currentPlayer;
-				break;
-			case 2: 
-				board[0][2] = currentPlayer;
-				break;
-			case 3: 
-				board[0][4] = currentPlayer;
-				break;
+				if (functions::isSpaceTaken(board, rows, cols, 0, 0)) {
+					board[0][0] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 2:
+				if (functions::isSpaceTaken(board, rows, cols, 0, 2)) {
+					board[0][2] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 3:
+				if (functions::isSpaceTaken(board, rows, cols, 0, 4)) {
+					board[0][4] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 4:
+				if (functions::isSpaceTaken(board, rows, cols, 2, 0)) {
+					board[2][0] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 5:
+				if (functions::isSpaceTaken(board, rows, cols, 2, 2)) {
+					board[2][2] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 6:
+				if (functions::isSpaceTaken(board, rows, cols, 2, 4)) {
+					board[2][4] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 7:
+				if (functions::isSpaceTaken(board, rows, cols, 4, 0)) {
+					board[4][0] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 8:
+				if (functions::isSpaceTaken(board, rows, cols, 4, 2)) {
+					board[4][2] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			case 9:
+				if (functions::isSpaceTaken(board, rows, cols, 4, 4)) {
+					board[4][4] = currentPlayer;
+					validInput = false;
+					break;
+				}
+				else {
+					counter++;
+					continue;
+				}
+			}
+		} while (validInput);
+		
+		gameLimit++;
+		if (gameLimit == 9) {
+			std::cout << "It is a draw...";
 		}
 
 	} while (counter < 100);
 
 };
+
+
